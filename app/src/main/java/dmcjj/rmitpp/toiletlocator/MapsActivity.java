10aa24bc02ac6a6b78b2926014fb1b0ca518900e
tessarm <30427858+tessarm@users.noreply.github.com>
2017-08-14 08:40:58 +1000

    more stuff
    more stuff

--- app/src/main/java/dmcjj/rmitpp/toiletlocator/MapsActivity.java
+++ app/src/main/java/dmcjj/rmitpp/toiletlocator/MapsActivity.java
@@ -40,8 +40,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng melbourne = new LatLng(-37.814, 144.9632);
+        //mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
+        
+        LatLng locationhere new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(locationhere).title("Your current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(locationhere));
     }
 }
