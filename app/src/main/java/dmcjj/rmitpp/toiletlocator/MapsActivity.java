cb464b4e1f11122f618b15568ae80b737cab6556
tessarm <30427858+tessarm@users.noreply.github.com>
2017-08-18 15:50:02 +1000

    Find user location
    Added permission checks and onmapactivity functionality to centre the map on the user's location and place a marker on that location

--- app/src/main/java/dmcjj/rmitpp/toiletlocator/MapsActivity.java
+++ app/src/main/java/dmcjj/rmitpp/toiletlocator/MapsActivity.java
@@ -1,9 +1,20 @@
 package dmcjj.rmitpp.toiletlocator;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +22,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static int PERMISSIONCHECK = 1;
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +38,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONCHECK);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        LatLng melbourne = new LatLng(-37.814, 144.9632);
-        mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            Log.d("PERM", "Permission denied");
+            return;
 
-        LatLng toilet1 = new LatLng(-37.80312731, 144.9497264);
-        mMap.addMarker(new MarkerOptions().position(toilet1).title("Queensberry st"));
+        }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+        Log.d("qwer", "qwe");
 
-        // Add a marker in Sydney and move the camera
-        //LatLng melbourne = new LatLng(-37.814, 144.9632);
-        //mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
+        Location locations = locationManager.getLastKnownLocation(provider);
+        List<String> providerList = locationManager.getAllProviders();
+        if (null != locations && null != providerList && providerList.size() > 0) {
+            double longitude = locations.getLongitude();
+            double latitude = locations.getLatitude();
+
+
+
+            mMap = googleMap;
+
+            LatLng melbourne = new LatLng(locations.getLatitude(), locations.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+
+
+
+
+
+
+
+            // Add a marker in Sydney and move the camera
+            //LatLng melbourne = new LatLng(-37.814, 144.9632);
+            //mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
 
 //        LatLng locationhere new LatLng(location.getLatitude(), location.getLongitude());
 //        mMap.addMarker(new MarkerOptions().position(locationhere).title("Your current location"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(locationhere));
-    }
-}
+        }
+
+
+
+
+
+    } }
