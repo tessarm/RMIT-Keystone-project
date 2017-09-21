d8c4c0e05e13c9b15fba71af86ce1bae142dd2b6
JasonMelb <jasonofthepacific@gmail.com>
2017-09-21 11:58:46 +1000

    fix login twitter

--- app/src/main/java/dmcjj/rmitpp/toiletlocator/activity/Login.java
+++ app/src/main/java/dmcjj/rmitpp/toiletlocator/activity/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -85,19 +86,21 @@ public class Login extends AppCompatActivity
         });
 
 
-//        mLoginButton = (TwitterLoginButton) findViewById(R.id.button_twitter_login);
-//        mLoginButton.setCallback(new Callback<TwitterSession>() {
-//            @Override
-//            public void success(Result<TwitterSession> result) {
-//                Log.d(TAG, "twitterLogin:success" + result);
-//                handleTwitterSession(result.data);
-//            }
-//
-//            @Override
-//            public void failure(TwitterException exception) {
-//                Log.w(TAG, "twitterLogin:failure", exception);
-//                updateUI(null);
-// above is code from the firebase/twitter documentation - need help
+
+        mLoginButton = (TwitterLoginButton) findViewById(R.id.button_twitter_login);
+        mLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d("login", "twitterLogin:success" + result);
+                handleTwitterSession(result.data);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.w("login", "twitterLogin:failure", exception);
+                updateUI(null);
+            }
+        });
     }
 
     @Override
